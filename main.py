ad669e1bd689bd8358e5f0524af6203f88500b67
GitPushPullLegs <jose.aguilar.6694@gmail.com>
2021-02-25 18:27:32 -0800

    Added 20 file cap since MFT won't take more.

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ import keyring
 import os
 import glob
 from datetime import datetime
+from win10toast import ToastNotifier
 
 if __name__ == '__main__':
+    toaster = ToastNotifier()
     timestamp = datetime.now().strftime("%Y%m%d-%H%M%S")
-    mft = Client(host=keyring.get_password('mcs', 'mft_host'))
-    mft.login(username=keyring.get_password('mcs', 'ae-user'), password=keyring.get_password('mcs', 'ae-pass'))
+
     input_files = [x.replace("\"", "") for x in sys.argv[1:]]  # Omit main.py.
     mft_files = []
     for file in input_files:
@@ -18,9 +19,16 @@ if __name__ == '__main__':
         else:
             mft_files.append(file)
 
-    password = generate_mnemonic_password()
-    link = mft.create_file_share(share_type=Client.ShareType.send, files=mft_files, password=password, subject=f'MFT Context Menu Share {timestamp}')
-    output_file = open(os.path.join(os.path.split(input_files[0])[0], 'MFT Context Menu Share {timestamp}.txt'), 'w')
-    output_file.write(f"Link: {link}\n")
-    output_file.write(f"Password: {password}\n")
-    output_file.close()
+    if len(mft_files) > 20:
+        toaster.show_toast(title="MFT Error",
+                           msg="Cannot share more than 20 files at a time.",
+                           threaded=True)
+    else:
+        mft = Client(host=keyring.get_password('mcs', 'mft_host'))
+        mft.login(username=keyring.get_password('mcs', 'ae-user'), password=keyring.get_password('mcs', 'ae-pass'))
+        password = generate_mnemonic_password()
+        link = mft.create_file_share(share_type=Client.ShareType.send, files=mft_files, password=password, subject=f'MFT Context Menu Share {timestamp}')
+        output_file = open(os.path.join(os.path.split(input_files[0])[0], 'MFT Context Menu Share {timestamp}.txt'), 'w')
+        output_file.write(f"Link: {link}\n")
+        output_file.write(f"Password: {password}\n")
+        output_file.close()
